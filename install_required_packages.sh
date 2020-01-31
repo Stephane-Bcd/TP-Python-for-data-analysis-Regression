456e145b9314a10bc5ff0788725d5c8f11f6f35c
Stephane Boucaud <stephane.boucaud@protonmail.com>
2020-01-31 21:56:40 +0100

    rzquired packages for jupyter notebook

--- install_required_packages.sh
+++ install_required_packages.sh
@@ -1,4 +1,15 @@
 # required packages to install
 # to launch with bash
 
+# install python
 sudo apt install python3 python3-pip
+sudo apt-get -y install python2.7
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-dev
+
+# config for jupyter notebook
+# launch it using: jupyter notebook .
+sudo apt-get -y install ipython ipython-notebook
+sudo pip install jupyter
+
+
