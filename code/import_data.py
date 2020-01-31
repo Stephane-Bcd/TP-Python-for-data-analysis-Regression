b296773b964100dad6e7d5d7db76f77fb0bab474
Stephane Boucaud <stephane.boucaud@protonmail.com>
2020-01-31 22:33:15 +0100

    starting to transform data

--- code/import_data.py
+++ code/import_data.py
@@ -1,11 +1,50 @@
+
+from datetime import datetime
+
 # Load the Pandas libraries with alias 'pd' 
 import pandas as pd 
-# Read data from file 'filename.csv' 
-# (in the same directory that your python process is based)
-# Control delimiters, rows, column names with read_csv (see later) 
-data = pd.read_csv("../incident_event_log.csv") 
-# Preview the first 5 lines of the loaded data 
-print("Data imported with pandas:")
-print(data.head())
+
+'''
+	Function to read CSV file and return pandas dataframe
+'''
+def import_data(path="../incident_event_log.csv"):
+	# Read data from file
+	data = pd.read_csv(path) 
+	
+	# Preview the first 5 lines of the loaded data 
+	print("Data imported with pandas:")
+	print(data.head())
+	
+	return data
+
+'''
+	Function to filter and tranform current data to make it more interesting in ou case
+'''
+
+def transform_data(df):
+	return df
+
+
+'''
+	This section is about testing all current functions
+'''
+
+data = import_data()
+
+# example of dates transformations into datetime
+data_transformed = pd.DataFrame({
+	"opened_at_datetime": [datetime.strptime(x, "%d/%m/%Y %H:%M") for x in data["opened_at"]],
+	"opened_at": data["opened_at"]
+})
+
+print(data_transformed.head(100))
+
+# Now transforming all dates
+
+
+
+
+
+
 
 
